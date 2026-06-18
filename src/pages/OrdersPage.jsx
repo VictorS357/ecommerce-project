2d8420f96fe76a33302f55293f2f4a2cf4e93034
VictorS357 <victorfransousa357@gmail.com>
2026-06-18 16:48:55 -0300

    move logos and icons to src folder and create imports

--- src/pages/OrdersPage.jsx
+++ src/pages/OrdersPage.jsx
@@ -1,4 +1,5 @@
 import { Header } from '../components/Header';
+import buyAgainIcon from '../assets/images/icons/buy-again.png'
 import './OrdersPage.css';
 
 export function OrdersPage() {
@@ -9,38 +10,6 @@ export function OrdersPage() {
       <title>Orders</title>
 
       <Header />
-
-      <div className="header">
-        <div className="left-section">
-          <a href="/" className="header-link">
-            <img className="logo"
-              src="images/logo-white.png" />
-            <img className="mobile-logo"
-              src="images/mobile-logo-white.png" />
-          </a>
-        </div>
-
-        <div className="middle-section">
-          <input className="search-bar" type="text" placeholder="Search" />
-
-          <button className="search-button">
-            <img className="search-icon" src="images/icons/search-icon.png" />
-          </button>
-        </div>
-
-        <div className="right-section">
-          <a className="orders-link header-link" href="/orders">
-
-            <span className="orders-text">Orders</span>
-          </a>
-
-          <a className="cart-link header-link" href="/checkout">
-            <img className="cart-icon" src="images/icons/cart-icon.png" />
-            <div className="cart-quantity">3</div>
-            <div className="cart-text">Cart</div>
-          </a>
-        </div>
-      </div>
 
       <div className="orders-page">
         <div className="page-title">Your Orders</div>
@@ -82,7 +51,7 @@ export function OrdersPage() {
                   Quantity: 1
                 </div>
                 <button className="buy-again-button button-primary">
-                  <img className="buy-again-icon" src="images/icons/buy-again.png" />
+                  <img className="buy-again-icon" src={buyAgainIcon} />
                   <span className="buy-again-message">Add to Cart</span>
                 </button>
               </div>
@@ -110,7 +79,7 @@ export function OrdersPage() {
                   Quantity: 2
                 </div>
                 <button className="buy-again-button button-primary">
-                  <img className="buy-again-icon" src="images/icons/buy-again.png" />
+                  <img className="buy-again-icon" src={buyAgainIcon} />
                   <span className="buy-again-message">Add to Cart</span>
                 </button>
               </div>
@@ -161,7 +130,7 @@ export function OrdersPage() {
                   Quantity: 2
                 </div>
                 <button className="buy-again-button button-primary">
-                  <img className="buy-again-icon" src="images/icons/buy-again.png" />
+                  <img className="buy-again-icon" src={buyAgainIcon} />
                   <span className="buy-again-message">Add to Cart</span>
                 </button>
               </div>
