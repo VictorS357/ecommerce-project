6264024a949cc161fb392c3114d3592fec33f330
VictorS357 <victorfransousa357@gmail.com>
2026-06-19 09:30:44 -0300

    use data from backend to generate HTML

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,18 @@
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 import { Header } from '../components/Header';
-import { products } from '../../starting code/data/products';
 import checkmark from '../assets/images/icons/checkmark.png';
 import './HomePage.css';
 
 export function HomePage() {
-  axios.get('http://localhost:3000/api/products')
-    .then(res => {
-      console.log(res.data);
-    });
-
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    axios.get('http://localhost:3000/api/products')
+      .then(res => {
+        setProducts(res.data);
+      });
+  }, []);
+  
   return (
     <>
       <link rel="icon" type="image/svg+xml" href="/images/home-favicon.png" />
