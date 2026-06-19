0abbb52240f1ff7a2b22b5403fa10d95d854be69
VictorS357 <victorfransousa357@gmail.com>
2026-06-19 09:48:41 -0300

    lift cart up and share it between pages

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,19 +4,13 @@ import { Header } from '../components/Header';
 import checkmark from '../assets/images/icons/checkmark.png';
 import './HomePage.css';
 
-export function HomePage() {
+export function HomePage({cart}) {
   const [products, setProducts] = useState([]);
-  const [cart, setCart] = useState([]);
 
   useEffect(() => {
     axios.get('/api/products')
       .then(res => {
         setProducts(res.data);
-      });
-
-    axios.get('/api/cart-items')
-      .then(res => {
-        setCart(res.data);
       });
   }, []);
   
