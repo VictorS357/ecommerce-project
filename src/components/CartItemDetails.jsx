771ba371afb33f59c164ccd76ef2881d00c81018
VictorS357 <victorfransousa357@gmail.com>
2026-07-21 17:07:14 -0300

    make update feature on checkout page work

--- src/components/CartItemDetails.jsx
+++ src/components/CartItemDetails.jsx
@@ -1,10 +1,29 @@
 import axios from "axios";
 import { formatMoney } from "../utils/money";
+import { useState } from "react";
 
 export default function CartItemDetails({ cartItem, loadCart }) {
+  const [update, setUpdate] = useState(false);
+  const [quantity, setQuantity] = useState(cartItem.quantity);
+
   const deleteCartItem = async () => {
     await axios.delete(`/api/cart-items/${cartItem.productId}`);
     await loadCart();
+  }
+
+  const updateCartItem = async () => {
+    setUpdate(true);
+    if(update) {
+      await axios.put(`/api/cart-items/${cartItem.productId}`, {
+        quantity: Number(quantity)
+      });
+      await loadCart();
+      setUpdate(false);
+    }
+  };
+
+  const saveQuantity = (event) => {
+    setQuantity(event.target.value);
   }
 
   return (
@@ -21,9 +40,18 @@ export default function CartItemDetails({ cartItem, loadCart }) {
         </div>
         <div className="product-quantity">
           <span>
-            Quantity: <span className="quantity-label">{cartItem.quantity}</span>
+            Quantity:
+            {update ? <input
+                className="update-text"
+                type="text"
+                style={{ width: "50px" }} 
+                value={quantity} 
+                onChange={saveQuantity}
+              /> :
+              <span className="quantity-label">{cartItem.quantity}</span>}
           </span>
-          <span className="update-quantity-link link-primary">
+          <span className="update-quantity-link link-primary"
+            onClick={updateCartItem}>
             Update
           </span>
           <span className="delete-quantity-link link-primary"
