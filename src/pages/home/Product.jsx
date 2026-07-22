38f52691694c185b8594ad7a2239e08aa85e293f
VictorS357 <victorfransousa357@gmail.com>
2026-07-22 09:29:49 -0300

    create integration test for Product component

--- src/pages/home/Product.jsx
+++ src/pages/home/Product.jsx
@@ -29,6 +29,7 @@ export default function Product({ product, loadCart }) {
     <div className="product-container">
       <div className="product-image-container">
         <img className="product-image"
+          data-testid="product-image"
           src={product.image} />
       </div>
 
@@ -38,7 +39,9 @@ export default function Product({ product, loadCart }) {
 
       <div className="product-rating-container">
         <img className="product-rating-stars"
-          src={`images/ratings/rating-${product.rating.stars * 10}.png`} />
+          src={`images/ratings/rating-${product.rating.stars * 10}.png`} 
+          data-testid="rating-image"
+        />
         <div className="product-rating-count link-primary">
           {product.rating.count}
         </div>
